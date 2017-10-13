3922252e6d09255677755779d0a6b0348f1a66c8
profoundblue <profoundblue@163.com>
2017-10-13 09:44:36 +0800

    0039++

--- doc/mysql/init_mysql_schema.sql
+++ doc/mysql/init_mysql_schema.sql
@@ -54,7 +54,7 @@ create table if not exists `administrator_role_rel_permissions`(
 drop table if exists `administrator_roles`;
 create table if not exists `administrator_roles`(
 	`id` bigint unsigned auto_increment not null,
-	`code` varchar(10) character set utf8 collate utf8_bin not null,
+	`code` varchar(20) character set utf8 collate utf8_bin not null,
 	`name` varchar(50) character set utf8 collate utf8_bin not null,
 	`add_time` timestamp default current_timestamp,
 	unique(`name`),
--- shop/library/administrator.class.php
+++ shop/library/administrator.class.php
@@ -38,24 +38,73 @@ class Administrator extends Mysql {
 	}
 	
 	/**
-	 * public array function page(integer $page_num, integer $page_length = 20)
+	 * public integer function get_role_num(void)
 	 */
-	public function page(int $page_num, int $page_length = 20): array {
-		$offset = ($page_num - 1) * $page_length;
-		return $this->field(['id', 'code', 'name'])->order(['id'=>'asc'])->limit($page_length, $offset)->select();
+	public function get_role_num(): int {
+		$ends = $this->field(['role_num'=>'count(*)'])->table(['administrator_roles'])->select();
+		return isset($ends[0]) ? $ends[0]['role_num'] : -1;
 	}
 	
 	/**
+	 * public array function get_role_page(integer $page_num = 1, integer $page_length = 20)
 	 */
-	public function get_role_page(int $permission_id, int $page_num, int $page_length = 20): array {
+	public function get_role_page(int $page_num = 1, int $page_length = 20): array {
 		$offset = ($page_num - 1) * $page_length;
-		return $this->field()->table(['administrator_roles'=>'r'])->join()->where()->order()->limit($page_length, $offset)->select();
+		$this->field(['ar.id', 'ar.code', 'ar.name', 'admin_num'=>'count(`a`.`id`)', 'ar.add_time']);
+		$this->table(['ar'=>'administrator_roles'])->join(['a'=>'administrators', 'ar.id'=>'a.role_id'], 'left');
+		$this->order(['ar.id'=>'asc'])->group(['ar.id'])->limit($page_length, $offset);
+		return $this->select();
 	}
 	
 	/**
-	 * public array function get_admin_page(integer $permission_id, integer $page_num, integer $page_length = 20)
+	 * public array function get_role_item(integer $role_id)
 	 */
-	public function get_admin_page(int $permission_id, int $page_num, int $page_length = 20): array {
+	public function get_role_item(int $role_id): array {
+		$this->field(['ar.id', 'ar.code', 'ar.name', 'admin_num'=>'count(`a`.`id`)', 'ar.add_time']);
+		$this->table(['ar'=>'administrator_roles'])->join(['a'=>'administrators', 'ar.id'=>'a.role_id'], 'left');
+		$ends = $this->where(['ar.id'=>(string)$role_id])->select();
+		$ends['permissions'] = $this->get_role_permissions($role_id);
+		return $ends;
 	}
+	
+	/**
+	 * public array function get_role_permissions(integer $role_id)
+	 */
+	public function get_role_permissions(int $role_id): array {
+		$this->field(['ap.id', 'ap.code', 'ap.name'])->table(['arrp'=>'administrator_role_rel_permissions']);
+		$this->join(['ap'=>'administrator_permissions', 'arrp.permission_id'=>'ap.id']);
+		$this->where(['arrp.role_id'=>(string)$role_id])->order(['ap.id'=>'asc']);
+		return $this->select();
+	}
+	
+	/**
+	 * public bool function add_role(string $code, string $name)
+	 */
+	public function add_role(string $code, string $name): bool {
+		$datas = ['code'=>$code, 'name'=>$name];
+		$end = $this->table(['administrator_roles'])->add($datas);
+		return $end > 0 ? true : false;
+	}
+	
+	/**
+	 * public bool function modify_role(integer $role_id, string $code, string $name)
+	 */
+	public function modify_role(int $role_id, string $code, string $name): bool {
+		if(1 == $role_id) return false; // $role_id=1, $code='super_admin', no modify!
+		$datas = ['code'=>$code, 'name'=>$name];
+		$end = $this->table(['administrator_roles'])->where(['id'=>(string)$role_id])->modify($datas);
+		return $end > 0 ? true : false;
+	}
+	
+	/**
+	 * public integer function delete_role(int $role_id)
+	 */
+	public function delete_role(int $role_id): int {
+		if(1 == $role_id) return -2; // $role_id=1, $code='super_admin', no delete!
+		$datas = $this->field(['admin_num'=>'count(*)'])->table(['administrators'])->where(['role_id'=>(string)$role_id])->select();
+		if(isset($datas[0]) && $datas[0]['admin_num'] > 0) return -3; // role including admins!
+		return $this->table(['administrator_roles'])->where(['id'=>(string)$role_id])->delete(); // -1: database error!
+	}
+	
 	//
 }
